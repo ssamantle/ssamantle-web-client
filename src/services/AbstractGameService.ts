b394570819615690bb8315264d108650cf281e1f
Hepheir <hepheir@gmail.com>
2026-04-08 14:11:42 +0900

    feat(src.services): add `AbstractGameService` 클래스 구현

---
+++ src/services/AbstractGameService.ts
@@ -0,0 +1,98 @@
+import { UserSession } from "./UserService";
+import {
+  GameState,
+  GameInfo,
+  UserInfo,
+  Guess,
+  GameService,
+  PRE_GAME,
+  POST_GAME,
+  IN_GAME,
+} from "./GameService";
+
+export abstract class AbstractGameService implements GameService {
+  private gameInfo: GameInfo = {
+    startAt: null,
+    endAt: null,
+    users: [],
+  };
+  private guessHistory: Guess[] = [];
+  private userSession: UserSession | null = null;
+
+  /** 서버에서 게임 정보를 가져옵니다. */
+  protected abstract fetchGameInfo(): Promise<GameInfo>;
+
+  /** 서버에서 사용자 단어 제출 기록을 가져옵니다. */
+  protected abstract fetchGuessHistory(
+    userSession: UserSession,
+  ): Promise<Guess[]>;
+
+  /** 서버에 단어를 제출하고 단어 정보를 가져옵니다. */
+  protected abstract fetchGuess(
+    userSession: UserSession,
+    word: string,
+  ): Promise<Guess>;
+
+  async setUserSession(userSession: UserSession): Promise<void> {
+    this.userSession = userSession;
+    this.guessHistory = await this.fetchGuessHistory(userSession);
+  }
+
+  async syncGameInfo(): Promise<void> {
+    this.gameInfo = {
+      ...this.gameInfo,
+      ...(await this.fetchGameInfo()),
+    };
+  }
+
+  getGameStartTime(): number | null {
+    return this.gameInfo.startAt;
+  }
+
+  getGameEndTime(): number | null {
+    return this.gameInfo.endAt;
+  }
+
+  getCurrentGameState(now: number = Date.now()): GameState {
+    const { startAt, endAt } = this.gameInfo;
+    if (startAt === null || now < startAt) {
+      return PRE_GAME;
+    }
+    if (endAt !== null && now < endAt) {
+      return IN_GAME;
+    }
+    return POST_GAME;
+  }
+
+  getMyGuessHistory(): Guess[] {
+    return this.guessHistory;
+  }
+
+  getMyInfo(): UserInfo | null {
+    if (!this.userSession) return null;
+    return this.findUserByName(this.userSession.username);
+  }
+
+  getMyBestGuess(): Guess | null {
+    const guesses = this.getMyGuessHistory();
+    if (guesses.length === 0) return null;
+    return guesses.reduce((g1, g2) => {
+      return g1.similarity > g2.similarity ? g1 : g2;
+    });
+  }
+
+  listUsers(): UserInfo[] {
+    return this.gameInfo.users;
+  }
+
+  findUserByName(name: string): UserInfo | null {
+    return this.gameInfo.users.find((u) => u.name === name) ?? null;
+  }
+
+  async submitGuess(word: string): Promise<Guess | null> {
+    if (this.userSession === null) {
+      throw new Error("USER_SESSION_NOT_FOUND");
+    }
+    return await this.fetchGuess(this.userSession, word);
+  }
+}
