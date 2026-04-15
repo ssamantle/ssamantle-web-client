661cf4379297f96bb630778f1fc740f857b677c4
Hepheir <hepheir@gmail.com>
2026-04-15 19:23:06 +0900

    chore: 서버 호스트명을 현재 클라이언트와 같은 것으로 사용

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import { Configuration, GamesV1Api } from "@ssamantle/sdk-typescript";
 
 const config = new Configuration({
-  basePath: "http://localhost:8000",
+  basePath: `http://${window.location.hostname}:8000`,
 });
 
 export const gamesApi = new GamesV1Api(config);
